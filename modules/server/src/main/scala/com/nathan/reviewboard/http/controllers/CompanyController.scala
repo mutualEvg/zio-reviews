326a5ec1e9e8cf4127db00c5c40d0dda48acb892
Evgeny Losev <evgeny.losev@bp.com>
2024-05-25 17:57:15 -0400

    add .gitignore remove garbage

--- modules/server/src/main/scala/com/nathan/reviewboard/http/controllers/CompanyController.scala
+++ modules/server/src/main/scala/com/nathan/reviewboard/http/controllers/CompanyController.scala
@@ -40,22 +40,22 @@ class CompanyController private (service: CompanyService) extends BaseController
 //        }
 //    }
 
-  val create: ServerEndpoint[Any, Task] = createEndpoint.serverLogicSuccess { req =>
-    service.create(req)
+  val create: ServerEndpoint[Any, Task] = createEndpoint.serverLogic { req =>
+    service.create(req).either
   }
 
   val getAll: ServerEndpoint[Any, Task] =
-    getAllEndpoint.serverLogicSuccess(_ => service.getAll)
+    getAllEndpoint.serverLogic(_ => service.getAll.either)
 
   val getById: ServerEndpoint[Any, Task] =
-    getByIdEndpoint.serverLogicSuccess { id =>
+    getByIdEndpoint.serverLogic { id =>
       ZIO
         .attempt(id.toLong)
         .flatMap(service.getById)
         .catchSome {
           case _: NumberFormatException =>
           service.getBySlug(id)
-        }
+        }.either
     }
 
 
--- modules/server/src/main/scala/com/nathan/reviewboard/http/controllers/ReviewController.scala
+++ modules/server/src/main/scala/com/nathan/reviewboard/http/controllers/ReviewController.scala
@@ -14,13 +14,13 @@ class ReviewController private (reviewService: ReviewService)
     with ReviewEndpoints {
 
   val create: ServerEndpoint[Any, Task] =
-    createEndpoint.serverLogicSuccess(req => reviewService.create(req, -1L /* TODO add user id */))
+    createEndpoint.serverLogic(req => reviewService.create(req, -1L /* TODO add user id */).either)
 
   val getById: ServerEndpoint[Any, Task] =
-    getByIdEndpoint.serverLogicSuccess(id => reviewService.getById(id))
+    getByIdEndpoint.serverLogic(id => reviewService.getById(id).either)
 
   val getByCompanyId: ServerEndpoint[Any, Task] =
-    getByCompanyIdEndpoint.serverLogicSuccess(companyId => reviewService.getByCompanyId(companyId))
+    getByCompanyIdEndpoint.serverLogic(companyId => reviewService.getByCompanyId(companyId).either)
 
   override val routes: List[ServerEndpoint[Any, Task]] =
     List(create, getById, getByCompanyId)
